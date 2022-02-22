849c2a57a4efefe779cb486e1b311acd13fe6bec
Konrath <36306272+Ikeaven@users.noreply.github.com>
2022-02-22 13:10:18 +0100

    filters endpoints

--- CRM_Epic_Events/crm/urls.py
+++ CRM_Epic_Events/crm/urls.py
@@ -6,14 +6,23 @@ from crm.views import (
     ClientFilteredBySales,
     ClientCustomerList,
     ContractListBySales,
-    ContractListByClient,
+    ContractListByClientID,
+    ClientFilteredByName,
+    ClientFilteredByEmail,
+    ContractListByClientName,
+    ContractListByClientEmail,
+    ContractListByDate,
+    ContractListByAmount,
+    EventFilteredByClientName,
+    EventFilteredByClientEmail,
+    EventFilteredByDate,
 )
 
 router = DefaultRouter()
-router.register(r"company", CompanyViewSet, basename="company")
-router.register(r"client", ClientViewSet, basename="client")
-router.register(r"contract", ContractViewSet, basename="contract")
-router.register(r"event", EventViewSet, basename="event")
+router.register(r"companies", CompanyViewSet, basename="company")
+router.register(r"clients", ClientViewSet, basename="client")
+router.register(r"contracts", ContractViewSet, basename="contract")
+router.register(r"events", EventViewSet, basename="event")
 
 urlpatterns = [
     path(
@@ -24,7 +33,15 @@ urlpatterns = [
     path(
         "clients/customer/<int:int>",
         ClientCustomerList.as_view(),
-        name="client-customer",
+        name="client-is-customer",
+    ),
+    path(
+        "client/name/<str:name>", ClientFilteredByName.as_view(), name="client-by-name"
+    ),
+    path(
+        "client/email/<str:email>",
+        ClientFilteredByEmail.as_view(),
+        name="client-by-email",
     ),
     path(
         "contracts/seller/<int:pk>",
@@ -32,9 +49,44 @@ urlpatterns = [
         name="contract-list-by-sales",
     ),
     path(
-        "contracts/client/<int:pk>",
-        ContractListByClient.as_view(),
-        name="contract_list_by_client",
+        "contracts/client/id/<int:pk>",
+        ContractListByClientID.as_view(),
+        name="contract-list-by-client-id",
+    ),
+    path(
+        "contracts/client/name/<str:name>",
+        ContractListByClientName.as_view(),
+        name="contract-list-by-client-name",
+    ),
+    path(
+        "contracts/client/email/<str:email>",
+        ContractListByClientEmail.as_view(),
+        name="contract-list-by-client-email",
+    ),
+    path(
+        "contracts/date/<str:date>",
+        ContractListByDate.as_view(),
+        name="contract-list-by-date",
+    ),
+    path(
+        "contracts/amount/<str:operator>/<int:amount>",
+        ContractListByAmount.as_view(),
+        name="contract-list-by-amount",
+    ),
+    path(
+        "events/client/name/<str:name>",
+        EventFilteredByClientName.as_view(),
+        name="event-list-by-client-name",
+    ),
+    path(
+        "events/client/email/<str:email>",
+        EventFilteredByClientEmail.as_view(),
+        name="event-list-by-client-email",
+    ),
+    path(
+        "events/date/<str:date>",
+        EventFilteredByDate.as_view(),
+        name="event-list-by-date",
     ),
 ]
 
