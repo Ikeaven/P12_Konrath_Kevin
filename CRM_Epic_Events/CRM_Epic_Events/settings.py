97476fbcc0577f3f56665947a6b8ea8f1d246699
Konrath <36306272+Ikeaven@users.noreply.github.com>
2022-02-27 20:57:23 +0100

    debug true

--- CRM_Epic_Events/CRM_Epic_Events/settings.py
+++ CRM_Epic_Events/CRM_Epic_Events/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9qk)d5u7&v6j*+0-^lewxl2u-w=e658y+p7_!7gxmyydk^4_$8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
